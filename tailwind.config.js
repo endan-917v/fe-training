446bd253176c4b1ecc532c4c9be1bdacef9dbdc0
endan-dev <lsalamante@yondu.com>
2023-11-29 16:52:31 +0800

    Improve Tailwind theme and add new image
    Refactor the Tailwind theme to extend color options and add a new
    boxShadow style. This enhances the visual aesthetics and customization
    capabilities of the project. Additionally, a new image file "iphone.png"
    has been added to the public directory. These changes improve the user
    interface and provide better design options.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          50: '#F9F5FF',
-          100: '#F4EBFF',
-          500: '#9E77ED',
-          600: '#7F56D9',
-          700: '#6941C6'
-        }
-      },
-      boxShadow: {
-        xs: '0px 1px 2px 0px rgba(16, 24, 40, 0.05)'
-      }
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    50: '#F9F5FF',
+                    100: '#F4EBFF',
+                    500: '#9E77ED',
+                    600: '#7F56D9',
+                    700: '#6941C6',
+                },
+            },
+            boxShadow: {
+                xs: '0px 1px 2px 0px rgba(16, 24, 40, 0.05)',
+                '3xl': '10.87081px 10.87081px 21.74162px -1.35885px rgba(16, 24, 40, 0.08), 21.74162px 21.74162px 43.48325px -2.7177px rgba(16, 24, 40, 0.20)',
+            },
+        },
     },
-  },
-  plugins: ['prettier-plugin-tailwindcss'],
+    plugins: ['prettier-plugin-tailwindcss'],
 }

29faf1fd9c4ed5eb73710bf639395b21d0d15fcb
endan-dev <lsalamante@yondu.com>
2023-11-29 14:34:43 +0800

    Refactor tailwind.config.js to update primary color shades and add xs box shadow (#12)
    Update the primary color shades in the tailwind.config.js file to
    include a lighter shade (100). Additionally, introduce an xs box shadow
    variant with specific rgba values to ensure consistent styling across
    components. These changes enhance the visual appeal and improve
    accessibility of the UI elements throughout the application.
    
    Fixes #12

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,14 @@ export default {
       colors: {
         primary: {
           50: '#F9F5FF',
+          100: '#F4EBFF',
           500: '#9E77ED',
           600: '#7F56D9',
           700: '#6941C6'
         }
+      },
+      boxShadow: {
+        xs: '0px 1px 2px 0px rgba(16, 24, 40, 0.05)'
       }
     },
   },

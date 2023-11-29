955e8c4dd276807f0ddcc468588a44ce2a12a514
endan-dev <lsalamante@yondu.com>
2023-11-29 15:52:55 +0800

    Refactor Button component to allow dynamic child content
    This change modifies the Button component to accept child content
    through the `children` prop. Previously, it only rendered the static
    text "Sign up" as the button label. The addition of the `children` prop
    enables custom content within the button, enhancing its flexibility and
    reusability. This change is applicable in scenarios where clients
    require different text or other elements inside the button component. No
    issues are associated with this commit.

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -1,20 +1,24 @@
+import { ReactNode } from 'react'
 import { cn } from '../../lib/utils'
 
 export default function Button({
     variant = 'primary',
+    children,
 }: {
     variant?: 'primary' | 'mono'
+    children: ReactNode
 }) {
     const defaultClasses =
-        'px-5 py-3 lg:px-7 lg:py-4 rounded-xl text-sm w-full lg:w-auto font-medium'
+        'px-5 py-3 lg:px-7 lg:py-4 rounded-xl text-sm w-full lg:w-auto font-medium flex gap-2 items-center justify-center'
     const usedClasses = cn(
         {
             'bg-primary-600 text-white': variant === 'primary',
         },
         {
-            'bg-white text-gray-700 border border-gray-300 shadow-xs': variant === 'mono',
+            'bg-white text-gray-700 border border-gray-300 shadow-xs':
+                variant === 'mono',
         },
         defaultClasses
     )
-    return <button className={usedClasses}>Sign up</button>
+    return <button className={usedClasses}>{children}</button>
 }

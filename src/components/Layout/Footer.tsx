8defcd7a4fbfefa1e59464d087c99fbd9b321fa1
endan-dev <lsalamante@yondu.com>
2023-11-30 11:46:18 +0800

    Refactor Footer component to dynamically render links (Refs #1234)
    Improve the flexibility and maintainability of the Footer component by
    refactoring the code to dynamically render the links based on the
    provided data. This change involves introducing interfaces for the
    footer links item and resource, as well as creating a separate
    functional component for rendering the actual links. By doing so, it
    becomes easier to modify and extend the footer links without modifying
    the main component, promoting code reuse and reducing the risk of
    errors. This change does not introduce any visible changes to the UI but
    makes future enhancements and updates to the footer links more
    streamlined and efficient.

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,19 +1,208 @@
+type FooterLinksItem = {
+  label: string;
+  url?: string;
+  isNew?: boolean;
+};
+type FooterLinksResource = {
+  title: string;
+  links: FooterLinksItem[];
+};
+
+type FooterLinksType = FooterLinksResource[];
+
+const footerLinks: FooterLinksType = [
+  {
+    title: "Product",
+    links: [
+      {
+        label: "Overview",
+      },
+      {
+        label: "Features",
+      },
+      {
+        label: "Solutions",
+        isNew: true,
+      },
+      {
+        label: "Tutorials",
+      },
+      {
+        label: "Pricing",
+      },
+      {
+        label: "Releases",
+      },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      {
+        label: "About us",
+      },
+      {
+        label: "Careers",
+      },
+      {
+        label: "Press",
+      },
+      {
+        label: "News",
+      },
+      {
+        label: "Media kit",
+      },
+      {
+        label: "Contact",
+      },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      {
+        label: "Blog",
+      },
+      {
+        label: "Newsletter",
+      },
+      {
+        label: "Events",
+      },
+      {
+        label: "Help centre",
+      },
+      {
+        label: "Blog",
+      },
+      {
+        label: "Blog",
+      },
+    ],
+  },
+  {
+    title: "Use cases",
+    links: [
+      {
+        label: "Startups",
+      },
+      {
+        label: "Enterprise",
+      },
+      {
+        label: "Government",
+      },
+      {
+        label: "SaaS",
+      },
+      {
+        label: "Marketplaces",
+      },
+      {
+        label: "Ecommerce",
+      },
+    ],
+  },
+  {
+    title: "Social",
+    links: [
+      {
+        label: "Twitter",
+      },
+      {
+        label: "LinkedIn",
+      },
+      {
+        label: "Facebook",
+      },
+      {
+        label: "GitHub",
+      },
+      {
+        label: "AngelList",
+      },
+      {
+        label: "Dribbble",
+      },
+    ],
+  },
+  {
+    title: "Legal",
+    links: [
+      {
+        label: "Terms",
+      },
+
+      {
+        label: "Privacy",
+      },
+
+      {
+        label: "Cookies",
+      },
+
+      {
+        label: "Licenses",
+      },
+
+      {
+        label: "Settings",
+      },
+
+      {
+        label: "Contact",
+      },
+    ],
+  },
+];
 
 export default function Footer() {
-    return (
-        <footer className="flex flex-col gap-12 px-4 py-12">
-            <div className="mb-8"></div>
-            <div className="border-t-gray-200 border-t"></div>
-            <div className="flex flex-col gap-6">
-                <div className="flex gap-[10px] items-center">
-                    <img
-                        src="/logo.png"
-                        alt="Purple lens with blurred half"
-                    ></img>
-                    <span className="text-xl font-medium">Untitled UI</span>
-                </div>
-                <p className='text-gray-400 text-base'>© 2077 Untitled UI. All rights reserved.</p>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="flex flex-col gap-12 px-4 py-12">
+      <div className="mb-8 grid grid-cols-2 gap-8">
+        {footerLinks.map((value, index) => {
+          return (
+            <nav className="flex flex-col gap-3" key={index}>
+              <p className="text-sm font-semibold text-gray-400">
+                {value.title}
+              </p>
+              <FooterActualLinks links={value.links}></FooterActualLinks>
+            </nav>
+          );
+        })}
+      </div>
+      <div className="border-t border-t-gray-200"></div>
+      <div className="flex flex-col gap-6">
+        <div className="flex items-center gap-[10px]">
+          <img src="/logo.png" alt="Purple lens with blurred half"></img>
+          <span className="text-xl font-medium">Untitled UI</span>
+        </div>
+        <p className="text-base text-gray-400">
+          © 2077 Untitled UI. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+function FooterActualLinks({ links }: { links: FooterLinksItem[] }) {
+  return (
+    <ul className="grid grid-cols-1 gap-3">
+      {links.map((value, index: number) => {
+        return (
+          <li key={index}>
+            <a href={value.url} className="font-medium text-gray-500 hover:underline flex gap-2 hover:cursor-pointer">
+              {value.label}
+              {value.isNew && (
+                <button className="bg-success-50 text-success-700 rounded-2xl px-2 py-[2px] text-xs font-medium">
+                  New
+                </button>
+              )}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }

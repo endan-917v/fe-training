a037d16a1e0fea74f4a07f72d29649885edff753
endan-dev <lsalamante@yondu.com>
2023-12-01 14:47:13 +0800

    Add desktop Nav Menu

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,24 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  // NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  // NavigationMenuViewport,
+  navigationMenuTriggerStyle,
+} from "../Common/NavigationMenu";
+
 export default function Header() {
   return (
-    <header className="flex h-[72px] px-4 py-5 lg:h-20 justify-between items-center lg:px-24 lg:py-6">
-      <div className="flex h-8 items-center justify-center gap-[10px]">
-        <img src="/logo.png" alt="Purple lens with blurred half"></img>
-        <span className="text-xl font-medium">Untitled UI</span>
+    <header className="flex h-[72px] items-center justify-between px-4 py-5 lg:h-20 lg:px-24 lg:py-6">
+      <div className="flex gap-10">
+        <div className="flex h-8 items-center justify-center gap-[10px]">
+          <img src="/logo.png" alt="Purple lens with blurred half"></img>
+          <span className="text-xl font-medium">Untitled UI</span>
+        </div>
+        <HeaderNavMenu />
       </div>
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -23,3 +38,50 @@ export default function Header() {
     </header>
   );
 }
+
+export const HeaderNavMenu = () => {
+  return (
+    <div className="hidden lg:block">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <a href="/">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </a>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Products</NavigationMenuTrigger>
+            <NavigationMenuContent className="grid grid-rows-3 grid-cols-2 lg:w-96">
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Spark</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Inbox</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Insight</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Resolve</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Connect</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Integrate</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Resources</NavigationMenuTrigger>
+            <NavigationMenuContent className="grid grid-rows-3 grid-cols-2 lg:w-96">
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Blog</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Newsletter</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Events</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Help centre</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Tutorials</NavigationMenuLink>
+              <NavigationMenuLink className="py-4 px-6 bg-white text-gray-500 font-medium text-sm">Support</NavigationMenuLink>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <a href="/">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Pricing
+              </NavigationMenuLink>
+            </a>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+};

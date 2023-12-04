3a097e84ebd9a53881ea6dfba8e81fc34f3b61b6
endan-dev <lsalamante@yondu.com>
2023-12-04 09:44:32 +0800

    feat: Refactor Header component to improve mobile navigation
    The Header component has been refactored to improve the mobile
    navigation functionality.
    
    - The mobile navigation menu button has been moved inside a Sheet
    component for better accessibility.
    - The mobile navigation menu now utilizes the HeaderNavMenu component
    for consistent styling and structure.
    - The mobile navigation menu has been updated to display a grid layout
    with a maximum width of 300px on medium and large screens.

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -10,34 +10,27 @@ import {
   navigationMenuTriggerStyle,
 } from "../Common/NavigationMenu";
 
+import {
+  Sheet,
+  SheetContent,
+  // SheetDescription,
+  SheetHeader,
+  // SheetTitle,
+  SheetTrigger,
+} from "../Common/Sheet";
+
 export default function Header() {
   return (
-    <header className="flex h-[72px] items-center justify-between px-4 py-5 lg:h-20 lg:px-24 lg:py-6 md:max-lg:px-28">
+    <header className="flex h-[72px] items-center justify-between px-4 py-5 md:max-lg:px-28 lg:h-20 lg:px-24 lg:py-6">
       <div className="flex gap-10">
         <div className="flex h-8 items-center justify-center gap-[10px]">
           <img src="/logo.png" alt="Purple lens with blurred half"></img>
           <span className="text-xl font-medium">Untitled UI</span>
         </div>
-        <HeaderNavMenu />
+        <HeaderNavMenu className="hidden lg:block" />
       </div>
-      <button className="block lg:hidden">
-        {/* hamburger menu button */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-        >
-          <path
-            d="M3 12H21M3 6H21M3 18H21"
-            stroke="#344054"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
-      </button>
+      {/* mobile nav header */}
+      <MobileHeaderNavMenu />
       <button className="hidden lg:block">
         {/* user profile picture button */}
         <img src="/faq-avatars/Avatar-2.png" alt="..." className="h-10 w-10" />
@@ -46,11 +39,11 @@ export default function Header() {
   );
 }
 
-export const HeaderNavMenu = () => {
+export const HeaderNavMenu = ({ className }: { className?: string }) => {
   return (
-    <div className="hidden lg:block">
+    <div className={className}>
       <NavigationMenu>
-        <NavigationMenuList>
+        <NavigationMenuList className="flex-col items-baseline lg:flex-row lg:items-center">
           <NavigationMenuItem>
             <NavigationMenuLink
               href="/"
@@ -61,7 +54,7 @@ export const HeaderNavMenu = () => {
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuTrigger>Products</NavigationMenuTrigger>
-            <NavigationMenuContent className="grid grid-cols-2 grid-rows-3 lg:w-96">
+            <NavigationMenuContent className="grid w-[300px] grid-cols-2 grid-rows-3 md:max-lg:w-80 lg:w-96">
               <NavigationMenuLink
                 href="/"
                 className="bg-white px-6 py-4 text-sm font-medium text-gray-500"
@@ -102,7 +95,7 @@ export const HeaderNavMenu = () => {
           </NavigationMenuItem>
           <NavigationMenuItem>
             <NavigationMenuTrigger>Resources</NavigationMenuTrigger>
-            <NavigationMenuContent className="grid grid-cols-2 grid-rows-3 lg:w-96">
+            <NavigationMenuContent className="grid w-[300px] grid-cols-2 grid-rows-3 md:max-lg:w-80 lg:w-96">
               <NavigationMenuLink
                 href="/"
                 className="bg-white px-6 py-4 text-sm font-medium text-gray-500"
@@ -152,5 +145,34 @@ export const HeaderNavMenu = () => {
         </NavigationMenuList>
       </NavigationMenu>
     </div>
+  );
+};
+
+export const MobileHeaderNavMenu = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className="block lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M3 12H21M3 6H21M3 18H21"
+            stroke="#344054"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </SheetTrigger>
+      <SheetContent className="w-full bg-white/90 md:bg-white">
+        <SheetHeader>
+          <HeaderNavMenu />
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
   );
 };
